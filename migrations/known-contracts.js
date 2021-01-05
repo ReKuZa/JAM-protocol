d646eb82a917d8b0e81adb76b9077b91d59dfc66
Shing Ho Tan <talenteyedev@gmail.com>
2021-01-05 04:52:47 -0800

    Update ropsten pools address

--- migrations/known-contracts.js
+++ migrations/known-contracts.js
@@ -13,14 +13,30 @@ module.exports = {
     rinkeby: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
     kovan: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
   },
+  FRAX: {
+    mainnet: '',
+    ropsten: '0x868034ef400b0950f3b1921a6d33d6d9c35634d2',
+  },
+  DSD: {
+    mainnet: '0xbd2f0cd039e0bfcf88901c98c0bfac5ab27566e3',
+    ropsten: '0x3cb5f3be4856a00f5d37d05699d0138cf631c0c6',
+    kovan: '0xbd2f0cd039e0bfcf88901c98c0bfac5ab27566e3',
+  },
+  ESD: {
+    mainnet: '0x36f3fd68e7325a35eb768f1aedaae9ea0689d723',
+    ropsten: '0x8da4b92647c7ffeafda80d41799a7cad5a6f12ec',
+  },
   DAI: {
     mainnet: '0x6b175474e89094c44da98b954eedeac495271d0f',
+    ropsten: '0xad6d458402f60fd3bd25163575031acdce07538d',
   },
   SUSD: {
     mainnet: '0x57Ab1E02fEE23774580C119740129eAC7081e9D3',
   },
   USDC: {
     mainnet: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    ropsten: '0x07865c6e87b9f70255377e024ace6630c1eaa37f',
+    kovan: '0xdcfab8057d08634279f8201b55d311c2a67897d2',
   },
   USDT: {
     mainnet: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
